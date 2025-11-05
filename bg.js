46d86babffc85a66453447a9b5f55f1573a73762
Under1ines <3038969270@qq.com>
2025-11-05 12:01:23 +0800

    Site updated: 2025-11-05 12:01:23

--- bg.js
+++ bg.js
@@ -12,8 +12,3 @@ if (window.innerWidth < 768) {
   document.querySelector('#web_bg').setAttribute('style', `background-image: ${desktopBgImageUrl};position: fixed;width: 100%;height: 100%;z-index: -1;background-size: cover;`);
 }
 
-// 设置banner的背景图片为空
-document.querySelector("#banner").setAttribute('style', 'background-image: none');
-
-// 设置banner的.mask背景颜色透明
-document.querySelector("#banner .mask").setAttribute('style', 'background-color: rgba(0,0,0,0)');
